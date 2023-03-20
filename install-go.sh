0379f5be7a2314bbb993e667bac51d864a272e5c
nickzer0 <nickpearce12@googlemail.com>
2023-03-20 16:29:06 +0000

    Create install-go.sh

---
+++ install-go.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Installs latest version of Go.
+
+if (( $EUID != 0 )); then
+    echo "[+] Please run as root."
+    exit
+fi
+echo -ne "\r[+] Installing latest Version of Go..."
+goUrl=$(curl https://go.dev/dl/ -s | grep "download" | grep ".tar.gz" | head -n 1 | cut -d '"' -f 4)
+goVer=$(echo $goUrl | cut -d '/' -f 3)
+wget https://go.dev$goUrl -q
+rm -rf /usr/local/go && tar -C /usr/local -xzf $goVer &> /dev/null
+echo -ne "\r\033[K                        "
+echo -ne '\r[+] Installed: '$goVer'' | cut -d "-" -f 1
