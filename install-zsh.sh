02ad5af218dd94618eece3c588384afed13fb288
Nick Pearce <me@nickzero.co.uk>
2025-01-30 13:53:15 +0000

    Updated

--- install-zsh.sh
+++ install-zsh.sh
@@ -8,4 +8,6 @@ USER_HOME=$(eval echo ~${SUDO_USER:-$USER})
 
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="gianu"/g' "$USER_HOME/.zshrc"
 
+chsh -s /bin/zsh
+
 echo "Zsh installed and theme set to 'gianu' for user: ${SUDO_USER:-$USER}"
