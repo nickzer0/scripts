16d32f0330d8b646aca74ea92e25c34c048e61d6
Nick Pearce <me@nickzero.co.uk>
2025-01-30 13:51:04 +0000

    Added install-zsh

---
+++ install-zsh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update && apt install zsh git curl -y
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+USER_HOME=$(eval echo ~${SUDO_USER:-$USER})
+
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="gianu"/g' "$USER_HOME/.zshrc"
+
+echo "Zsh installed and theme set to 'gianu' for user: ${SUDO_USER:-$USER}"
